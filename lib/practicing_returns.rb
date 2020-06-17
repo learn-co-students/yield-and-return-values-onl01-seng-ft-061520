26ee3aca30f75ee4b5ac5382b6ef08fae8b31a62
devWRM <mcwen@comcast.net>
2020-06-17 17:38:12 -0400

    Done.

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -2,11 +2,27 @@ require 'pry'
 
 def hello(array)
   i = 0
+  catch = []
   while i < array.length
-    yield(array[i])
+
+    catch << yield(array[i])
     i += 1
   end
+  catch
 end
 
+hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
+ 
+################ hello method without a block ####################
 
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+# array = ["Zim", "Zom", "Xim"]
+
+# def hello(array)
+#   i = 0
+#   while i < array.length
+#     puts "Hi #{array[i]}"
+#     i += 1
+#   end
+# end
+
+# hello(array)
